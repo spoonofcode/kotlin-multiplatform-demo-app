8c71697406acc0d4aa322243bd84dde6aa7bb68e
spoonofcode <102907783+spoonofcode@users.noreply.github.com>
2024-07-12 14:29:35 +0200

    #5 Add Voyager navigation with example (#9)

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,15 +1,60 @@
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import features.task.TaskOverview
-import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.compose.KoinContext
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.navigator.tab.CurrentTab
+import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabNavigator
+import tabs.home.HomeTab
+import tabs.settings.SettingsTab
 
 @Composable
-@Preview
 fun App() {
-    KoinContext {
-        MaterialTheme {
-            TaskOverview()
+    MaterialTheme {
+        TabNavigator(
+            tab = HomeTab
+        ) {
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                bottomBar = {
+                    BottomNavigation {
+                        TabNavigationItem(HomeTab)
+                        TabNavigationItem(SettingsTab)
+                    }
+                },
+                content = { CurrentTab() },
+            )
         }
     }
+}
+
+@Composable
+private fun RowScope.TabNavigationItem(tab: Tab) {
+    val tabNavigator: TabNavigator = LocalTabNavigator.current
+
+    BottomNavigationItem(
+        selected = tabNavigator.current == tab,
+        onClick = { tabNavigator.current = tab },
+        icon = {
+            tab.options.icon?.let { icon ->
+                Icon(
+                    painter = icon,
+                    contentDescription =
+                    tab.options.title
+                )
+            }
+        },
+        label = {
+            Text(
+                text = tab.options.title
+            )
+        }
+    )
 }
